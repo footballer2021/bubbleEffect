b1bcfd89c0fa24a21984f1482dde068fddf06036
footballer2021 <soccerzuki2013@gmail.com>
2022-04-16 22:35:17 +0900

    ドロワーのアニメーションを完成させました。

---
+++ app.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector(".hamburger");
+var navLinks = document.querySelector(".nav-links");
+var links = document.querySelectorAll(".nav-links li");
+hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("open");
+    links.forEach(function (link) {
+        link.classList.toggle("fade");
+    });
+});
